78fada38dfab467f4beeb19c9f2f9d29e568038c
KenzaBennani-Meziane <2111031@talnet.nl>
2020-03-03 11:05:44 +0100

    Uitwerking van de exercise

--- error_log.php
+++ error_log.php
@@ -1,8 +1,8 @@
 <?php
 function countDown($number) {
-	if ($number>10 || $number<0) {
-		error_log ("Error! Something went wrong...", 3, "error.log");
-	}
+    if ($number>10 || $number<0) {
+        error_log("Error! Something went wrong...",3,"error.log");
+    }
 }
 
 countDown(11);
